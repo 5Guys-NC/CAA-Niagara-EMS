355cd5b8d8cdcf7b24f0bdeb79e5fbe079c8c27c
BrianCulp <59587863+BrianCulp@users.noreply.github.com>
2020-03-02 17:34:44 -0500

    c

--- Views/EventViews/EventAttendanceTracking.xaml.cs
+++ Views/EventViews/EventAttendanceTracking.xaml.cs
@@ -1,27 +1,19 @@
-﻿using System;
+﻿using CAA_Event_Management.Data;
+using CAA_Event_Management.Models;
+using CAA_Event_Management.Utilities;
+using CAA_Event_Management.ViewModels;
+using CAA_Event_Management.Views.EventViews;
+using CAA_Event_Management.Views.Games;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using CAA_Event_Management.Models;
-using CAA_Event_Management.Data;
-using CAA_Event_Management.Utilities;
-using CAA_Event_Management.Views.Games;
-using System.Threading.Tasks;
-using CAA_Event_Management.ViewModels;
 /***********************************
-* Created By: Jon Yade
-* Edited By: Brian Culp, Jon Yade
-* ********************************/
+ * Created By: Jon Yade
+ * Edited By: Brian Culp
+ * ********************************/
 
 namespace CAA_Event_Management.Views.EventViews
 {
@@ -30,7 +22,7 @@ namespace CAA_Event_Management.Views.EventViews
         #region Startup - variables, repositories, methods
 
         private int questionCount = 0;
-        Event currentEvent;
+        Models.Event currentEvent;
         AttendanceTracking tracker = new AttendanceTracking();
         AttendanceItem survey = new AttendanceItem(); //may be able to delete this after testing
         List<EventItemDetails> ListOfEID = new List<EventItemDetails>();
@@ -318,14 +310,4 @@ namespace CAA_Event_Management.Views.EventViews
 
         #endregion
     }
-    #region  Survey Question Class
-
-    internal class SurveyQuestion
-    {
-        internal int ID { get; set; }
-        internal string questPhrase { get; set; } = "";
-        internal string questDataType { get; set; } = "";
-    }
-
-    #endregion
 }
